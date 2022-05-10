c83d950faddd8af161498d3377a0c9a0de9123fe
Fernando Echevarria Gianello <fernandoechevarriagianello@iMac-de-Fernando.local>
2022-05-10 15:03:26 +0200

    https://github.com/ferechegia/lab-dom-ironhack-cart.git

--- js/index.js
+++ js/index.js
@@ -2,23 +2,42 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
   //... your code goes here
+  console.log(product);
+  
+    const priceElement = product.querySelector('.price span');
+    const quantityElement = product.querySelector('.quantity input');
+    const subtotalNum = product.querySelector('.subtotal span');
+    const subtotal = price * quantity;
+    
+    const price = priceElement.innerText;
+    const quantity = quantityElement.value;
+    
+    subtotalNum.innerText = subtotal;
+    
+    return subtotal;
 }
+
+
+
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  const allProducts = document.querySelector('.product');
   // end of test
 
   // ITERATION 2
   //... your code goes here
-
+  for (let i = 0; i < allProducts.length; i += 1) {
+    updateSubtotal(allProducts[i]);
+  }
   // ITERATION 3
   //... your code goes here
+  const totalPrice = document.querySelector(`#total-value span`);
+  console.log(totalPrice);
 }
+
 
 // ITERATION 4
 
@@ -40,3 +59,9 @@ window.addEventListener('load', () => {
 
   //... your code goes here
 });
+
+
+
+
+
+
